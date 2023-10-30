ac0ffba526557d5dec929b3893c3c4fef44957ca
LUCIANOGFORTES02 <90663048+LUCIANOGFORTES02@users.noreply.github.com>
2023-10-30 12:32:23 -0300

    Add files via upload

---
+++ Exercicio4POO.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exercicio4poo;
+public class Exercicio4POO {
+
+    public static void main(String[] args) {
+       Invoice inv = new Invoice(1, "Mousepad", 10, 50.0f);
+        System.out.println("Valor total " + inv.getInvoiveAmount());
+    
+    }
+    
+}
