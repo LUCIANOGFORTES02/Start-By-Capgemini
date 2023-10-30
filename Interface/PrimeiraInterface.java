2f5e97dbdcffd6b8d54b41132e59db063512b808
LUCIANOGFORTES02 <90663048+LUCIANOGFORTES02@users.noreply.github.com>
2023-10-30 12:46:36 -0300

    Add files via upload

---
+++ Interface/PrimeiraInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeirainterface;
+
+/**
+ *
+ * @author Luciano
+ */
+public class PrimeiraInterface {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
